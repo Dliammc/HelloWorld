9f92f461e34d44fc5e367e536a45de124c05d014
D. Liam Mc. <dliammccraw@gmail.com>
2025-08-05 17:34:56 -0400

    Update hello.py
    Added new message

--- hello.py
+++ hello.py
@@ -2,4 +2,10 @@
 #08/05/2025
 # A simple python program to display a "Hello World" message
 
+# Daniel McCraw
+#08/05/2025
+# added "Welcome to Github!" message
+
 print("Hello World!")
+
+print("Welcome to Github!")
